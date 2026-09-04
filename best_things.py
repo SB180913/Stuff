a8b920682947711289b6a4b4e133f4839c083832
SB180913 <girlfrombklyn@icloud.com>
2026-09-04 21:24:38 +0000

    C

--- best_things.py
+++ best_things.py
@@ -7,6 +7,8 @@ movie = input("What is your favorite movie? ")
 song = input("What is your favorite song? ")
 food = input("What is your favorite food? ")
 
+print("\n")
+
 print(movie + " is a great film!")
 print("I love to eat " + food + "!")
 print("Never heard of " + song + ".")
