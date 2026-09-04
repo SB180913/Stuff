265f2a6134262ddc58f7149db9ea0d41460d53de
SB180913 <girlfrombklyn@icloud.com>
2026-09-04 21:16:11 +0000

    C

--- best_things.py
+++ best_things.py
@@ -3,9 +3,9 @@ Sophia Babayev, Section 10
 Best Things
 """
 
-movie = input("What is your favorite movie?")
-song = input("What is your favorite song?")
-food = input("What is your favorite food?")
+movie = input("What is your favorite movie? ")
+song = input("What is your favorite song? ")
+food = input("What is your favorite food? ")
 
 print(movie + " is a great film!")
 print("I love to eat " + food + "!")
