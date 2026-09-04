30b95200edb3cf07519ec8c94507dcc20d843434
SB180913 <girlfrombklyn@icloud.com>
2026-09-04 21:14:34 +0000

    C

---
+++ best_things.py
@@ -0,0 +1,13 @@
+"""
+Sophia Babayev, Section 10
+Best Things
+"""
+
+movie = input("What is your favorite movie?")
+song = input("What is your favorite song?")
+food = input("What is your favorite food?")
+
+print(movie + " is a great film!")
+print("I love to eat " + food + "!")
+print("Never heard of " + song + ".")
+
