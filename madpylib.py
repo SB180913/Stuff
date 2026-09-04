72bd178fc5d2fdfdd577db9ddc663132ed65af6b
SB180913 <girlfrombklyn@icloud.com>
2026-09-04 22:33:39 +0000

    C

---
+++ madpylib.py
@@ -0,0 +1,12 @@
+"""
+Sophia Babayev, Section 10
+Madpylib
+"""
+
+name = input("What is your name? ")
+sub = input("Provide a subject/noun. ")
+adj = input("Give an adjective.")
+verb = input("And a verb.")
+noun = input("Lastly another noun.")
+
+print("Hello " + name + ", we are going to have an amazing semester learning " + sub + ", it's going to be " + adj + " so don't worry if you need to " + verb + " from a " + noun + ".")
